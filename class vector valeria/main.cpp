4c5e239ed716776456ccc11f1b296b588ab0279d
valeriabukova <valeriasergeevna688@gmail.com>
2024-12-17 09:35:05 +0200

    Initial Commit

---
+++ class vector valeria/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class vector valeria
+//
+//  Created by Valeria  Bukova on 17.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
